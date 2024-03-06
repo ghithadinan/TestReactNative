68cd75c8526e6c1714f025f1292ebe9aad5550ce
Ghitha Dinan <ghithadinanhs@gmail.com>
2024-03-06 20:00:18 +0700

    init

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ import {name as appName} from './app.json';
 import LayoutComponent from './component/LayoutComponent';
 import FlexComponent from './component/FlexComponent';
 import FlexDirectionBasics from './component/FlexDirectionBasics';
+import DirectionLayout from './component/DirectionLayout';
 
 AppRegistry.registerComponent(appName, () => App);
 AppRegistry.registerComponent(appName, () => LayoutComponent);
 AppRegistry.registerComponent(appName, () => FlexComponent);
 AppRegistry.registerComponent(appName, () => FlexDirectionBasics);
+AppRegistry.registerComponent(appName, () => DirectionLayout);
